41e1434d4ff996d20c00fd53d21962835e53f9a4
Niklas Klein <mail@taig.io>
2025-09-29 17:13:35 +0200

    Add syntax package

---
+++ modules/core/src/main/scala/io/taig/data/syntax.scala
@@ -0,0 +1,11 @@
+package io.taig.data
+
+import io.taig.otter.data.Data
+
+object syntax:
+  def arr[A <: Data](values: A*): Data.Array[A] = Data.Array(values.toVector)
+
+  def obj[A <: Data](values: (String, A)*): Data.Object[A] =
+    Data.Object(values.toList)
+
+  extension (self: String) def :=[A <: Data](data: A): (String, A) = (self, data)
